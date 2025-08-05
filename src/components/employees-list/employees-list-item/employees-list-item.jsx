48cfeb8567cca66db87b6f4e633659ad1b5827ca
kirylkuchko <kirylkuchko@gmail.com>
2025-08-05 11:50:21 +0200

    Adeed state logic to EmployeesListItem in class component style

--- src/components/employees-list/employees-list-item/employees-list-item.jsx
+++ src/components/employees-list/employees-list-item/employees-list-item.jsx
@@ -1,32 +1,53 @@
+import { Component } from 'react';
 import './employees-list-item.css';
 
-export default function EmployeesListItem ({name, salary, promotion}){
-    
-    const getlistItemClassName = () => {
-        let listItemClassName = "employees-list-item list-group-item d-flex justify-content-between";
-        if (promotion) {
+export default class EmployeesListItem extends Component{
+    constructor(props) {
+        super(props);
+        this.name = this.props.name;
+        this.salary = this.props.salary;
+        this.state = {
+            promotion: this.props.promotion,
+            like: false
+        }
+    }
+
+    tooglePromotion = () => {
+        this.setState(({promotion}) => ({ promotion : !promotion}));
+    }
+
+    toogleLike = () => {
+        this.setState(({like}) => ({ like : !like}));
+    }
+
+    getlistItemClassName = () => {
+        let listItemClassName = 'employees-list-item list-group-item d-flex justify-content-between';
+        if (this.state.promotion) {
             listItemClassName += ' promotion';
+        }
+
+        if (this.state.like) {
+            listItemClassName += ' like';
         }
 
         return listItemClassName;
     }
 
-    return (
-        <li className={ getlistItemClassName() }>
-            <span className="list-group-item-label">{ name }</span>
-            <input type="text" className="list-group-item-input" defaultValue={ salary }/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
+    render() {
+        return (
+            <li className={ this.getlistItemClassName() }>
+                <span onClick={ this.toogleLike } className="list-group-item-label">{ this.name }</span>
+                <input type="text" className="list-group-item-input" defaultValue={ this.salary }/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button onClick={ this.tooglePromotion } type="button" className="btn-cookie btn-sm ">
+                        <i className="fas fa-cookie"></i>
+                    </button>
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+                    <button type="button"className="btn-trash btn-sm ">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+    )};
 }
